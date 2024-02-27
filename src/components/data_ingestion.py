47b1e1fe057189213ff49f516bb4fd2ac3407ee1
fazslgonzer <fazslgonzer@gmail.com>
2024-02-27 21:57:22 +0500

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,8 @@ from src.logger import logging
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -52,4 +53,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transfromation=DataTransformation()
+    data_transfromation.initiate_data_transformation(train_data,test_data)
+
